eecdbb097e75916c26dd7431a531c3193623d3cf
Lucky Mbithi <luckymbithi002@gmail.com>
2024-10-21 19:52:44 +0300

    ---

--- backend/controllers/auth-controller.js
+++ backend/controllers/auth-controller.js
@@ -95,7 +95,7 @@ export const login = async (req, res) => {
 
 export const logout = async (req, res) => {
   try {
-    req.cookie("jwt", "", { maxAge: 0 });
+    res.cookie("jwt", "", { maxAge: 0 });
     res.status(200).json({ message: "logged out successfully" });
   } catch (error) {
     res.status(500).json({ error: "Internal server error" });
