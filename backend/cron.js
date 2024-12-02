92d62dadc2551b075932fe5b67c3552a45424e6d
Lucky Mbithi <luckymbithi002@gmail.com>
2024-12-02 21:55:39 +0300

    ---

--- backend/cron.js
+++ backend/cron.js
@@ -4,10 +4,10 @@ import https from 'https'
 
 const URL = "https://travel-site-ueg1.onrender.com/api/destinations"
 
-const job = new cron.CronJob('10 * * * *', function () {
+const job = new cron.CronJob('1 * * * *', function () {
     https.get(URL, (res) => {
         if (res.statusCode === 200) {
-            console.log("request sent success");
+            console.log("CRON success");
         } else {
             console.log('request failed', res.statusCode);
         }
