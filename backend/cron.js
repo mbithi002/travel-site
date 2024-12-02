51e96fb04dc36edd7cc9505cf430de13aea3de5e
Lucky Mbithi <luckymbithi002@gmail.com>
2024-12-02 21:51:25 +0300

    ---

--- backend/cron.js
+++ backend/cron.js
@@ -2,7 +2,7 @@ import cron from 'cron'
 
 import https from 'https'
 
-const URL = "https://travel-site-ueg1.onrender.com/"
+const URL = "https://travel-site-ueg1.onrender.com/api/destinations"
 
 const job = new cron.CronJob('10 * * * *', function () {
     https.get(URL, (res) => {
